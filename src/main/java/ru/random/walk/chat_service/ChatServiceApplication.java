e01fcdde9fed28e5fd9dca6b5d62b3c256784342
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-09-28 13:18:35 +0300

    RANDOMWALK-41: change swagger url

--- src/main/java/ru/random/walk/chat_service/ChatServiceApplication.java
+++ src/main/java/ru/random/walk/chat_service/ChatServiceApplication.java
@@ -1,10 +1,17 @@
 package ru.random.walk.chat_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.random.walk.chat_service.config.properties.BrokerProps;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/chat", description = "Chat server url")
+        }
+)
 @EnableConfigurationProperties(BrokerProps.class)
 @SpringBootApplication
 public class ChatServiceApplication {
