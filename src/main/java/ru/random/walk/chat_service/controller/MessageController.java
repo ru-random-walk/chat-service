ef62f20beba5156a1f3966a7ab50396b451b9b2e
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-11-04 00:41:16 +0300

    RANDOMWALK-61: remove unused Page dto

--- src/main/java/ru/random/walk/chat_service/controller/MessageController.java
+++ src/main/java/ru/random/walk/chat_service/controller/MessageController.java
@@ -4,19 +4,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.random.walk.chat_service.controller.validation.PageableConstraint;
 import ru.random.walk.chat_service.model.dto.response.Message;
-import ru.random.walk.chat_service.model.dto.response.Page;
-import ru.random.walk.chat_service.model.dto.response.message.Type;
-import ru.random.walk.chat_service.model.dto.response.message.payload.Text;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("unused")
@@ -29,9 +28,9 @@ public class MessageController {
     @GetMapping("/list")
     public Page<Message> getHistory(
             Principal principal,
+            @PageableConstraint(maxPageSize = 100, message = "PageSize must be <= 100!")
+            Pageable pageable,
             @RequestParam @Schema(example = "919add90-0614-4ba4-b808-e423abfab4b6") UUID chatId,
-            @RequestParam @Schema(example = "0") long pageNumber,
-            @RequestParam @Schema(example = "1") long pageSize,
             @RequestParam(required = false) String message,
             @RequestParam(required = false)
             @Schema(example = "18:00 22-09-2024")
@@ -46,22 +45,12 @@ public class MessageController {
                         Get message history for [{}]
                         with login [{}]
                         with chat id [{}]
-                        with page number [{}]
-                        with page size [{}]
+                        with pageable [{}]
                         with message filter [{}]
                         with from filter [{}]
                         with to filter [{}]
                         """,
-                principal, principal.getName(), chatId, pageNumber, pageSize, message, from, to);
-        return new Page<>(List.of(
-                new Message(
-                        UUID.randomUUID(),
-                        new Text("Some text message"),
-                        Type.TEXT,
-                        chatId,
-                        true,
-                        LocalDateTime.of(2024, 9, 22, 18, 0)
-                )
-        ), 0, 0, 0);
+                principal, principal.getName(), chatId, pageable, message, from, to);
+        return null;
     }
 }
