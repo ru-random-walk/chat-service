280b60ddf7f38af221d0e7eb9f093d3cb3d4ca8b
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-11-04 14:26:07 +0300

    RANDOMWALK-61: fix mapper

--- src/main/java/ru/random/walk/chat_service/mapper/ChatMapper.java
+++ src/main/java/ru/random/walk/chat_service/mapper/ChatMapper.java
@@ -7,6 +7,6 @@ import ru.random.walk.chat_service.model.entity.ChatMemberEntity;
 
 @Mapper(componentModel = "spring")
 public interface ChatMapper {
-    @Mapping(source = "chatMember.id.chatId", target = "id")
+    @Mapping(source = "chatMemberEntity.id.chatId", target = "id")
     ChatDto chatMembertoChatDto(ChatMemberEntity chatMemberEntity);
 }
