d3e21ca3097abe694fb9159faf084d43a3fe39a4
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-11-04 14:47:12 +0300

    RANDOMWALK-61: add GlobalDateTimeFormat

--- src/main/java/ru/random/walk/chat_service/controller/MessageController.java
+++ src/main/java/ru/random/walk/chat_service/controller/MessageController.java
@@ -6,11 +6,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.random.walk.chat_service.controller.format.GlobalDateTimeFormat;
 import ru.random.walk.chat_service.controller.validation.PageableConstraint;
 import ru.random.walk.chat_service.model.dto.response.MessageDto;
 
@@ -28,18 +28,11 @@ public class MessageController {
     @GetMapping("/list")
     public Page<MessageDto> getHistory(
             Principal principal,
-            @PageableConstraint(maxPageSize = 100, message = "PageSize must be <= 100!")
-            Pageable pageable,
+            @PageableConstraint(maxPageSize = 100, message = "PageSize must be <= 100!") Pageable pageable,
             @RequestParam @Schema(example = "919add90-0614-4ba4-b808-e423abfab4b6") UUID chatId,
             @RequestParam(required = false) String message,
-            @RequestParam(required = false)
-            @Schema(example = "18:00 22-09-2024")
-            @DateTimeFormat(pattern = "HH:mm dd-MM-yyyy")
-            LocalDateTime from,
-            @RequestParam(required = false)
-            @Schema(example = "18:00 22-09-2024")
-            @DateTimeFormat(pattern = "HH:mm dd-MM-yyyy")
-            LocalDateTime to
+            @RequestParam(required = false) @GlobalDateTimeFormat LocalDateTime from,
+            @RequestParam(required = false) @GlobalDateTimeFormat LocalDateTime to
     ) {
         log.info("""
                         Get message history for [{}]
