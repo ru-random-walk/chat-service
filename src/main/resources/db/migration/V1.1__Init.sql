64f66275d0f99df4958e342aa40efd628a543bd3
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-11-07 21:53:56 +0300

    RANDOMWALK-61: fix migration

--- src/main/resources/db/migration/V1.1__Init.sql
+++ src/main/resources/db/migration/V1.1__Init.sql
@@ -11,6 +11,19 @@ $$
     end
 $$;
 
+-- Автоматический каст строки в тип chat_type
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1
+                       FROM pg_cast
+                       WHERE castsource = 'character varying'::regtype
+                         AND casttarget = 'chat.chat_type'::regtype) THEN
+            CREATE CAST (character varying AS chat.chat_type) WITH INOUT AS IMPLICIT;
+        END IF;
+    END
+$$;
+
 -- создание типа 'message_type', если он не существует
 do
 $$
@@ -22,6 +35,19 @@ $$
             create type chat.message_type as enum ('TEXT', 'REQUEST_FOR_WALK');
         end if;
     end
+$$;
+
+-- Автоматический каст строки в тип message_type
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1
+                       FROM pg_cast
+                       WHERE castsource = 'character varying'::regtype
+                         AND casttarget = 'chat.message_type'::regtype) THEN
+            CREATE CAST (character varying AS chat.message_type) WITH INOUT AS IMPLICIT;
+        END IF;
+    END
 $$;
 
 -- создание таблицы 'chat', если она не существует
@@ -43,11 +69,11 @@ create table if not exists chat.chat_members
 -- создание таблицы 'message', если она не существует
 create table if not exists chat.message
 (
-    id             uuid    default gen_random_uuid() primary key,
-    payload        jsonb                 not null,
-    type           chat.message_type     not null,
-    chat_id        uuid                  not null,
-    marked_as_read boolean default false not null,
-    sent_at        timestamp             not null,
+    id             uuid      default gen_random_uuid() primary key,
+    payload        jsonb                   not null,
+    type           chat.message_type       not null,
+    chat_id        uuid                    not null,
+    marked_as_read boolean   default false not null,
+    sent_at        timestamp default now() not null,
     foreign key (chat_id) references chat.chat (id)
 );
