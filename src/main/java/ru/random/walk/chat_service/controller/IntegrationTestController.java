a47691d7759fb2285a4ce0c82f339d1e12620863
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-12-12 20:07:38 +0300

    add endpoint

---
+++ src/main/java/ru/random/walk/chat_service/controller/IntegrationTestController.java
@@ -0,0 +1,28 @@
+package ru.random.walk.chat_service.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.random.walk.chat_service.service.ChatService;
+import ru.random.walk.dto.CreatePrivateChatEvent;
+
+@RestController
+@RequestMapping("/test")
+@Tag(name = "REST Integration Test Controller")
+@Slf4j
+@RequiredArgsConstructor
+public class IntegrationTestController {
+    private final ChatService chatService;
+
+    @Operation(summary = "Create private chat event - Kafka integration test")
+    @PostMapping("/create-private-chat-event")
+    public void createChat(@RequestBody CreatePrivateChatEvent event) {
+        log.info("Received test event: {}", event);
+        chatService.create(event);
+    }
+}
