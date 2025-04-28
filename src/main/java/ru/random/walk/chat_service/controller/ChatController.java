9933a65a138c378f2ad08d555aa0d31849eaa95e
Молчанов Максим <62752481+maksmolchdmitr@users.noreply.github.com>
2025-04-28 18:12:50 +0300

    RANDOMWALK-168: change docker-compose.yaml, add default uer authority for user controllers and add config (#27)

--- src/main/java/ru/random/walk/chat_service/controller/ChatController.java
+++ src/main/java/ru/random/walk/chat_service/controller/ChatController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ import java.util.UUID;
 @RequestMapping("/chat")
 @Tag(name = "REST Chat Controller")
 @AllArgsConstructor
+@PreAuthorize("hasAuthority('DEFAULT_USER')")
 public class ChatController {
     private final ChatService chatService;
     private final Authenticator authenticator;
--- src/main/java/ru/random/walk/chat_service/controller/MessageController.java
+++ src/main/java/ru/random/walk/chat_service/controller/MessageController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +29,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/message")
 @AllArgsConstructor
+@PreAuthorize("hasAuthority('DEFAULT_USER')")
 public class MessageController {
     private final MessageService messageService;
     private final Authenticator authenticator;
