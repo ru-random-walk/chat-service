716592b8bb7d36165bba600e37c4bc74a88b9cb3
maksmolch <maksmolchdmitr@gmail.com>
2025-04-30 20:55:12 +0300

    RANDOMWALK-163: add integration endpoint

--- src/main/java/ru/random/walk/chat_service/controller/IntegrationTestController.java
+++ src/main/java/ru/random/walk/chat_service/controller/IntegrationTestController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.random.walk.chat_service.service.AppointmentService;
 import ru.random.walk.chat_service.service.ChatService;
 import ru.random.walk.dto.CreatePrivateChatEvent;
+import ru.random.walk.dto.RequestedAppointmentStateEvent;
 
 @RestController
 @RequestMapping("/test")
@@ -20,11 +22,19 @@ import ru.random.walk.dto.CreatePrivateChatEvent;
 @PreAuthorize(value = "hasRole('TESTER')")
 public class IntegrationTestController {
     private final ChatService chatService;
+    private final AppointmentService appointmentService;
 
     @Operation(summary = "Create private chat event - Kafka integration test")
     @PostMapping("/create-private-chat-event")
     public void createChat(@RequestBody CreatePrivateChatEvent event) {
         log.info("Received test event: {}", event);
         chatService.create(event);
+    }
+
+    @Operation(summary = "Send requested appointment state event - Kafka integration test")
+    @PostMapping("/send-requested-appointment-state-event")
+    public void sendRequestedAppointmentStateEvent(RequestedAppointmentStateEvent event) {
+        log.info("Received requested appointment state event: {}", event);
+        appointmentService.updateState(event);
     }
 }
