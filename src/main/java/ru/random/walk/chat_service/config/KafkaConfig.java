d93d3c7986aded77471a00bf212260616f85f5c3
Молчанов Максим <62752481+maksmolchdmitr@users.noreply.github.com>
2025-05-07 15:23:02 +0300

    fix kafka listeners and config (#33)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/main/java/ru/random/walk/chat_service/config/KafkaConfig.java
+++ src/main/java/ru/random/walk/chat_service/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package ru.random.walk.chat_service.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,9 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import ru.random.walk.dto.CreatePrivateChatEvent;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,27 +22,43 @@ import java.util.Map;
 public class KafkaConfig {
     @Bean
     public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(KafkaProperties kafkaProperties) {
-        ConcurrentKafkaListenerContainerFactory<Long, CreatePrivateChatEvent> factory =
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerConfig(kafkaProperties));
         return factory;
     }
 
-    public ConsumerFactory<Long, CreatePrivateChatEvent> consumerConfig(KafkaProperties kafkaConfig) {
+    public ConsumerFactory<String, String> consumerConfig(KafkaProperties kafkaConfig) {
         Map<String, Object> properties = new HashMap<>();
-        JsonDeserializer<CreatePrivateChatEvent> deserializer = new JsonDeserializer<>(CreatePrivateChatEvent.class);
-        deserializer.addTrustedPackages("*");
-        deserializer.setRemoveTypeHeaders(false);
-        deserializer.setUseTypeMapperForKey(true);
 
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getConsumer().getBootstrapServers());
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfig.getConsumer().getGroupId());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafkaConfig.getConsumer().getMaxPollRecords());
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConfig.getConsumer().getAutoOffsetReset());
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaConfig.getConsumer().getEnableAutoCommit());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-        return new DefaultKafkaConsumerFactory<>(properties, new LongDeserializer(), deserializer);
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaProperties.getBootstrapServers()
+        );
+        configProperties.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+        configProperties.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+        return new DefaultKafkaProducerFactory<>(configProperties, new StringSerializer(), new StringSerializer());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(KafkaProperties kafkaProperties) {
+        return new KafkaTemplate<>(producerFactory(kafkaProperties));
     }
 }
--- src/main/java/ru/random/walk/chat_service/kafka/EventConsumer.java
+++ src/main/java/ru/random/walk/chat_service/kafka/EventConsumer.java
@@ -1,5 +1,7 @@
-package ru.random.walk.chat_service.service.kafka;
+package ru.random.walk.chat_service.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,16 +17,19 @@ import ru.random.walk.topic.EventTopic;
 @RequiredArgsConstructor
 public class EventConsumer {
     private final ChatService chatService;
+    private final ObjectMapper objectMapper;
     private final AppointmentService appointmentService;
 
     @KafkaListener(topics = EventTopic.CREATE_CHAT)
-    public void listen(CreatePrivateChatEvent event) {
+    public void listenCreatePrivateChatEvent(String message) throws JsonProcessingException {
+        var event = objectMapper.readValue(message, CreatePrivateChatEvent.class);
         log.info("Received create chat event: {}", event);
         chatService.create(event);
     }
 
     @KafkaListener(topics = EventTopic.REQUESTED_APPOINTMENT_STATE)
-    public void listen(RequestedAppointmentStateEvent event) {
+    public void listenRequestedAppointmentStateEvent(String message) throws JsonProcessingException {
+        var event = objectMapper.readValue(message, RequestedAppointmentStateEvent.class);
         log.info("Received requested appointment state event: {}", event);
         appointmentService.updateState(event);
     }
