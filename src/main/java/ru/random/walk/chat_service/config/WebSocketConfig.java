9eadd07bb3ba873c5ae87e8de75efaf730439a26
maksmolch <maksmolchdmitr@gmail.com>
2025-05-10 23:20:13 +0300

    fix

--- src/main/java/ru/random/walk/chat_service/config/WebSocketConfig.java
+++ src/main/java/ru/random/walk/chat_service/config/WebSocketConfig.java
@@ -1,8 +1,17 @@
 package ru.random.walk.chat_service.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +20,7 @@ import ru.random.walk.chat_service.config.properties.BrokerProps;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final BrokerProps brokerProps;
 
@@ -34,5 +44,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setUserDestinationBroadcast("/topic/unresolved-user")
                 .setUserRegistryBroadcast("/topic/log-user-registry");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(@NonNull ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    log.info("Pre send with message: {} and channel: {}", message, channel);
+                    // Access authentication header(s) and invoke accessor.setUser(user)
+//                    accessor.getFirstNativeHeader()
+//                    message.getHeaders().get()
+//                    accessor.setUser();
+                    accessor.setUser(accessor.getUser());
+                }
+                return message;
+            }
+        });
     }
 }
